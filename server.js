157cbdbed0be0c81193ece7ca6e5d65e8c3a2ab3
neillloool <nrc256@nyu.edu>
2017-05-14 17:15:51 -0400

    Shopping List v2 with Recipes

--- server.js
+++ server.js
@@ -48,9 +48,27 @@ app.post('/shopping-list', jsonParser, (req, res) => {
 });
 
 
+
+
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['dish', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.dish, req.body.ingredients);
+  res.sta
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
